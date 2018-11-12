70ad48f870fe949404cde554bbd843f91a59a505
KleberDevMen <klebersubcontas@gmail.com>
2018-11-12 15:01:17 +0000

    Crud Scrip

---
+++ app/views/scripts/_script.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! script, :id, :title, :content, :url_image, :created_at, :updated_at
+json.url script_url(script, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_141915) do
+ActiveRecord::Schema.define(version: 2018_11_12_144541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2018_11_12_141915) do
     t.string "title"
     t.string "lyric"
     t.string "artist"
+    t.string "url_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "scripts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
     t.string "url_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
